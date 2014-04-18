473cf3e98678e14bb6e3fa0c8eee52fc8e1de7b4
JD Hartley <jdhrtl@gmail.com>
2014-04-17 22:03:00 -0500

    start page => level; about page placeholder

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,3 +1,35 @@
-(function() {
 
+var GAME = (function() {
+    var state = [0, 0],
+        levels = ['chicago'],
+        answers = {
+            'chicago': [1,2,3]
+        };
+
+    return {
+        state: function(level, point) {
+            state = [level || 0, point || 0];
+            return GAME;
+        },
+        level: function() {
+            $('body').addClass('level-open');
+
+            $.ajax({
+                url: './assets/svg/' + levels[state[0] - 1] + '/' + state[1] + '.svg',
+                success: function(data) {
+                    $('#level').html(document.importNode(data.documentElement, true));
+                }
+            })
+        }
+    }
 })();
+
+$(function() {
+    $('#start button').click(function() {
+        GAME.state(1, 1).level();
+    });
+
+    $('.js-about-toggle').click(function() {
+        $('#about').fadeToggle();
+    });
+});
