bdf198ae03eaf55eb9934a6de4b92b36b47f050e
JD Hartley <jdhrtl@gmail.com>
2014-05-03 22:55:49 -0500

    start getting game play working
    store some data about each path; not sure how we will transform into sound yet
    random success message; might move this out into a cleaner, less hacky func idk
    switch from 'chicago' level because i dont have all the files for it yet

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,26 +1,49 @@
-
 var GAME = (function() {
     var state = [0, 0],
-        levels = ['chicago'],
+        levels = ['vic'],
+        guesses = [],
         answers = {
-            'chicago': [1,2,3]
+            'chicago': [[31,23,40]],
+            'vic': [[101,99,112]]
         },
 
-        _updateLevelSize = function() {
+        _scaleSVG = function() {
             $('#level svg').each(function() {
                 var i = $(this), w = i.attr('width'), h = i.attr('height'), cw = i.parents('#level').width();
                 i.css({width: cw, height: Math.floor(h / (w / cw))});
             });
         },
 
+        _pathInfo = function(path) {
+            var $path = $(path);
+
+            return $path.data('synaesthetic') || $path.data('synaesthetic', {
+                index: $path.parent().children().index($path),
+                color: parseInt($path.attr('fill').slice(1), 16)
+            }).data('synaesthetic');
+        },
         _pathEnter = function(e) {
-            console.log('ENTERING: ', this);
+            console.log('ENTERING: ', _pathInfo(this));
         },
         _pathLeave = function(e) {
-            console.log('LEAVING: ', this);
+            console.log('LEAVING: ', _pathInfo(this));
         },
         _pathClick = function(e) {
-            console.log('CLICKING: ', this);
+            var info = _pathInfo(this),
+                currentAnswer = answers[levels[state[0] - 1]][state[1] - 1];
+
+            console.log('CLICKING: ', info);
+
+            guesses.push(info['index']);
+
+            console.log('GUESS: ', guesses.toString());
+            console.log('ANSWER: ', currentAnswer.toString());
+
+            if ( guesses.toString() !== currentAnswer.slice(0, guesses.length).toString() ) {
+                guesses = [];
+            } else if ( guesses.length === currentAnswer.length ) {
+                GAME.advance();
+            }
         };
 
     return {
@@ -28,25 +51,32 @@ var GAME = (function() {
             state = [level || 0, point || 0];
             return GAME;
         },
+        advance: function() {
+            state[1]++ > 3 && ((state[1] = 0) || (state[0] = 1));
+            $('<div class="notice"/>').text((function(m,a) { return a[m.floor(m.random()*a.length)]; })(Math,
+                ['nice one!', 'sweet!', 'very nice', 'amazing!', 'good job!']
+            )).insertBefore('#level');
+            return GAME.level();
+        },
         level: function() {
             var svg = $.ajax('./assets/svg/' + levels[state[0] - 1] + '/' + state[1] + '.svg');
 
             $('body').addClass('level-open');
 
-            svg.done(function(data) {
-                $('#level').html(document.importNode(data.documentElement, true));
+            $.when(svg, $('#level svg').fadeOut()).done(function(svgDfr, animationDfr) {
+                $('#level').hide().html(document.importNode(svgDfr[0].documentElement, true)).fadeIn();
             });
 
-            svg.done(_updateLevelSize);
+            svg.done(_scaleSVG);
         },
         setup: function() {
-            $(window).resize(_updateLevelSize);
+            $(window).resize(_scaleSVG);
             $('#level')
                 .on('mouseenter', 'path', _pathEnter)
                 .on('mouseleave', 'path', _pathLeave)
                 .on('click',      'path', _pathClick);
 
-            $('<div class="instructions"/>').text('strike a chord').insertBefore('#level');
+            $('<div class="notice"/>').text('strike a chord').insertBefore('#level');
 
             return GAME;
         }
