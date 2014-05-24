0db85a65ac5315f77ea7361f288f9198ea443125
JD Hartley <jdhrtl@gmail.com>
2014-05-23 20:45:13 -0500

    Start in on cookies storing state'

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,226 +1,241 @@
-var GAME = (function() {
-    var state = [0, 0],
-        levels = ['jump', 'sf', 'vic', 'alicante', 'ns'],
-        answers = {
-            'jump':     [[65, 70, 74], [70, 74, 77], [64, 66, 68]],
-            'sf':       [[64, 68, 71], [70, 72, 74], [70, 72, 74]],
-            'vic':      [[68, 70, 72], [64, 66, 68], [70, 72, 74]],
-            'alicante': [[70, 72, 74], [72, 74, 76], [72, 74, 76]],
-            'ns':       [[67, 72, 76], [72, 76, 79], [79, 76, 72]]
-        },
-        guesses = [],
-        notes = {},
-        midi = [64, 65, 66, 67, 68, 70, 71, 72, 74, 76, 77, 79, 81],
-
-        SYNTH = (function() {
-            var synths = [],
-                currentSynth = 0,
-                sustainSynth = T('OscGen', { env: T('perc', { ar: true, r: 60 * 60 * 3 }) }).play();
-
-            for ( var i = 0; i < 20; i++ ) {
-                synths.push( T('OscGen', { env: T('perc', { ar: true }) }).play() );
-            }
-
-            return function(note, sustain) {
-                if ( 'undefined' !== typeof sustain ) {
-                    if ( sustain ) {
-                        return sustainSynth.noteOn( note, 13 );
-                    } else {
-                        sustainSynth.allSoundOff();
-                    }
-                }
-                currentSynth++ && currentSynth >= synths.length && (currentSynth = 0);
-                return synths[currentSynth].noteOn( note, 10 );
-            };
-        })(),
-
-        helpTimeout,
-
-        _scaleSVG = function() {
-            $('svg', '#level, #final').each(function() {
-                var i = $(this), w = i.attr('width'), h = i.attr('height'), cw = i.parents('#level, #final').width();
-                i.css({width: cw, height: Math.floor(h / (w / cw))});
-            });
-        },
-
-        _successMessage = function() {
-            var messages = [
-                'music to my ears!',
-                'sweet!',
-                'very nice!',
-                'amazing!',
-                'fantastic!'
-            ];
-            $('<div class="notice"/>').text(messages[ Math.floor(Math.random() * messages.length) ]).insertBefore('#level');
-        },
-
-        _generatePathInfo = function() {
-            $('#level path').each(function() {
-                var $path = $(this),
-                    index = $path.parent().children().index($path),
-                    color = parseInt(($path.attr('fill') || '').slice(1), 16) || false;
-
-                $path.data('synaesthetic', {
-                    index: index,
-                    color: color
-                });
-
-                if ( color ) {
-                    notes[color] = 0;
-                }
-            });
-
-            var index = 0,
-                splitAt = Math.ceil(Object.keys(notes).length / (midi.length - 1));
-
-            $.each(notes, function(color) {
-                notes[color] = midi[Math.floor(index++ / splitAt)];
-            });
-        },
-
-        _pathEnter = function(e) {
-            var info = $(this).data('synaesthetic');
-            console.log('ENTERING: ', info);
-            console.log('PLAY NOTE: ', notes[info.color]);
-            SYNTH( notes[info.color] );
-            console.log('IS NEXT NOTE:', (guesses.length ? guesses.toString() + ',' : '') + notes[info.color] === answers[levels[state[0] - 1]][state[1] - 1].slice(0, guesses.length + 1).toString() ? 'YES' : 'no');
-        },
-        _pathLeave = function(e) {
-            var info = $(this).data('synaesthetic');
-            console.log('LEAVING: ', info);
-            console.log('------------------------------------------------------------');
-        },
-        _pathClick = function(e) {
-            var info = $(this).data('synaesthetic'),
-                currentAnswer = answers[levels[state[0] - 1]][state[1] - 1];
-
-            console.log('CLICKING: ', info);
-
-            guesses.push(notes[info.color]);
-
-            console.log('GUESS: ', guesses.toString());
-            console.log('ANSWER: ', currentAnswer.toString());
-
-            if ( guesses.toString() !== currentAnswer.slice(0, guesses.length).toString() ) {
-                SYNTH( undefined, false );
-                guesses[0] && SYNTH( guesses[0] );
-                guesses[1] && SYNTH( guesses[1] );
-                guesses[2] && SYNTH( guesses[2] );
-                guesses = [];
-                $('[data-highlight]').removeAttr('data-highlight');
-            } else {
-                clearTimeout(helpTimeout);
-                $(this).attr('data-highlight', 'true').appendTo( $(this).parent() );
-                SYNTH( notes[info.color], true );
-            }
-
-            if ( guesses.length === currentAnswer.length ) {
-                GAME.advance();
-            }
-        };
-
-    return {
-        state: function(level, point) {
-            state = [level || 0, point || 0];
-            return GAME;
-        },
-        advance: function() {
-            guesses = [];
-            if ( state[1]++ < 3 ) {
-                return GAME.level(_successMessage);
-            }
-
-            $('#level').fadeOut(function() {
-                $(this).html($('<img/>').attr('src', 'assets/svg/' + levels[state[0] - 1] + '/final.jpg'))
-                    .fadeIn().one('click', function() {
-                        state[0]++;
-                        state[1] = 1;
-                        GAME.level(_successMessage);
-                    });
-            });
-            return GAME;
-        },
-        level: function(callback) {
-            $('body').addClass('level-open');
-
-            if ( 'undefined' === typeof levels[state[0] - 1] ) {
-                return GAME.end(callback);
-            }
-
-            $('body').addClass('whitenoise-fix');
-
-            var prevState = [state[0], state[1] - 1];
-            if ( prevState[1] <= 0 ) {
-                prevState[0]--;
-                prevState[1] = 1;
-            }
-
-            SYNTH( undefined, false );
-            if ( prevState[0] ) {
-                var lastAnswer = answers[levels[prevState[0] - 1]][prevState[1] - 1];
-                SYNTH( lastAnswer[0] );
-                SYNTH( lastAnswer[1] );
-                SYNTH( lastAnswer[2] );
-            }
-
-            var svg = $.ajax('assets/svg/' + levels[state[0] - 1] + '/' + state[1] + '.svg');
-            $.wait(prevState[0] ? 1000 : 0).then(function() {
-                $.when(svg, $('#level').fadeOut(prevState[0] ? undefined : 0)).done(function(svgDfr, animationDfr) {
-                    $('#level').html(document.importNode(svgDfr[0].documentElement, true)).fadeIn(function() {
-                        $('body').removeClass('whitenoise-fix');
-                        callback();
-                    });
-
-                    _generatePathInfo();
-                    _scaleSVG();
-                });
-            });
-        },
-        setup: function() {
-            $(window).resize(_scaleSVG);
-
-            $('#level')
-                .on('mouseenter', 'path', _pathEnter)
-                .on('mouseleave', 'path', _pathLeave)
-                .on('click',      'path', _pathClick);
-
-            $('#help button').click(function() {
-                $('body').addClass('whitenoise-fix');
-                $('#help').fadeOut(function() {
-                    $(this).remove();
-                    $('#level path').filter(function() {
-                        return answers.jump[0][0] === notes[$(this).data('synaesthetic').color];
-                    }).attr('data-highlight', 'true').first().click();
-                    $('body').removeClass('whitenoise-fix');
-                });
-            });
-
-            return GAME;
-        },
-        end: function(callback) {
-            $('#level').fadeOut(function() { SYNTH( undefined, false ) });
-            $('#final').addClass('open');
-            $.ajax('assets/svg/endgame.svg').done(function(data) {
-                $('#final').prepend(document.importNode(data.documentElement, true));
-                _scaleSVG();
-                callback();
-            });
-
-            return GAME;
-        },
-        help: function() {
-            helpTimeout = setTimeout(function() {
-                if ( ! $('#level').is(':visible') ) {
-                    return GAME.help();
-                }
-                $('#help').fadeIn();
-            }, 1000 * 15);
-        }
-    }
-})();
-
 $(function() {
-    $('#start button').click(function() {
+    window.GAME = (function() {
+        var state = [0, 0],
+            levels = ['jump', 'sf', 'vic', 'alicante', 'ns'],
+            answers = {
+                'jump':     [[65, 70, 74], [70, 74, 77], [64, 66, 68]],
+                'sf':       [[64, 68, 71], [70, 72, 74], [70, 72, 74]],
+                'vic':      [[68, 70, 72], [64, 66, 68], [70, 72, 74]],
+                'alicante': [[70, 72, 74], [72, 74, 76], [72, 74, 76]],
+                'ns':       [[67, 72, 76], [72, 76, 79], [79, 76, 72]]
+            },
+            guesses = [],
+            notes = {},
+            midi = [64, 65, 66, 67, 68, 70, 71, 72, 74, 76, 77, 79, 81],
+
+            SYNTH = (function() {
+                var synths = [],
+                    currentSynth = 0,
+                    sustainSynth = T('OscGen', { env: T('perc', { ar: true, r: 60 * 60 * 3 }) }).play();
+
+                for ( var i = 0; i < 20; i++ ) {
+                    synths.push( T('OscGen', { env: T('perc', { ar: true }) }).play() );
+                }
+
+                return function(note, sustain) {
+                    if ( 'undefined' !== typeof sustain ) {
+                        if ( sustain ) {
+                            return sustainSynth.noteOn( note, 13 );
+                        } else {
+                            sustainSynth.allSoundOff();
+                        }
+                    }
+                    currentSynth++ && currentSynth >= synths.length && (currentSynth = 0);
+                    return synths[currentSynth].noteOn( note, 10 );
+                };
+            })(),
+
+            helpTimeout,
+
+            _scaleSVG = function() {
+                $('svg', '#level, #final').each(function() {
+                    var i = $(this), w = i.attr('width'), h = i.attr('height'), cw = i.parents('#level, #final').width();
+                    i.css({width: cw, height: Math.floor(h / (w / cw))});
+                });
+            },
+
+            _successMessage = function() {
+                var messages = [
+                    'music to my ears!',
+                    'sweet!',
+                    'very nice!',
+                    'amazing!',
+                    'fantastic!'
+                ];
+                $('<div class="notice"/>').text(messages[ Math.floor(Math.random() * messages.length) ]).insertBefore('#level');
+            },
+
+            _generatePathInfo = function() {
+                $('#level path').each(function() {
+                    var $path = $(this),
+                        index = $path.parent().children().index($path),
+                        color = parseInt(($path.attr('fill') || '').slice(1), 16) || false;
+
+                    $path.data('synaesthetic', {
+                        index: index,
+                        color: color
+                    });
+
+                    if ( color ) {
+                        notes[color] = 0;
+                    }
+                });
+
+                var index = 0,
+                    splitAt = Math.ceil(Object.keys(notes).length / (midi.length - 1));
+
+                $.each(notes, function(color) {
+                    notes[color] = midi[Math.floor(index++ / splitAt)];
+                });
+            },
+
+            _pathEnter = function(e) {
+                var info = $(this).data('synaesthetic');
+                console.log('ENTERING: ', info);
+                console.log('PLAY NOTE: ', notes[info.color]);
+                SYNTH( notes[info.color] );
+                console.log('IS NEXT NOTE:', (guesses.length ? guesses.toString() + ',' : '') + notes[info.color] === answers[levels[state[0] - 1]][state[1] - 1].slice(0, guesses.length + 1).toString() ? 'YES' : 'no');
+            },
+            _pathLeave = function(e) {
+                var info = $(this).data('synaesthetic');
+                console.log('LEAVING: ', info);
+                console.log('------------------------------------------------------------');
+            },
+            _pathClick = function(e) {
+                var info = $(this).data('synaesthetic'),
+                    currentAnswer = answers[levels[state[0] - 1]][state[1] - 1];
+
+                console.log('CLICKING: ', info);
+
+                guesses.push(notes[info.color]);
+
+                console.log('GUESS: ', guesses.toString());
+                console.log('ANSWER: ', currentAnswer.toString());
+
+                if ( guesses.toString() !== currentAnswer.slice(0, guesses.length).toString() ) {
+                    SYNTH( undefined, false );
+                    guesses[0] && SYNTH( guesses[0] );
+                    guesses[1] && SYNTH( guesses[1] );
+                    guesses[2] && SYNTH( guesses[2] );
+                    guesses = [];
+                    $('[data-highlight]').removeAttr('data-highlight');
+                } else {
+                    clearTimeout(helpTimeout);
+                    $(this).attr('data-highlight', 'true').appendTo( $(this).parent() );
+                    SYNTH( notes[info.color], true );
+                }
+
+                if ( guesses.length === currentAnswer.length ) {
+                    GAME.advance();
+                }
+            };
+
+        return {
+            state: function(level, point) {
+                state = [level || 0, point || 0];
+                return GAME;
+            },
+            advance: function() {
+                guesses = [];
+                if ( state[1]++ < 3 ) {
+                    return GAME.level(_successMessage);
+                }
+
+                $('#level').fadeOut(function() {
+                    $(this).html($('<img/>').attr('src', 'assets/svg/' + levels[state[0] - 1] + '/final.jpg'))
+                        .fadeIn().one('click', function() {
+                            state[0]++;
+                            state[1] = 1;
+                            GAME.level(_successMessage);
+                        });
+                });
+                return GAME;
+            },
+            level: function(callback) {
+                $('body').addClass('level-open');
+
+                $('body').toggleClass('played', state[0] > 1 || state[1] > 1);
+
+                if ( 'undefined' === typeof levels[state[0] - 1] ) {
+                    return GAME.end(callback);
+                }
+
+                $.cookie('level', state[0]);
+                $.cookie('point', state[1]);
+
+                $('body').addClass('whitenoise-fix');
+
+                var prevState = [state[0], state[1] - 1];
+                if ( prevState[1] <= 0 ) {
+                    prevState[0]--;
+                    prevState[1] = 1;
+                }
+
+                SYNTH( undefined, false );
+                if ( prevState[0] ) {
+                    var lastAnswer = answers[levels[prevState[0] - 1]][prevState[1] - 1];
+                    SYNTH( lastAnswer[0] );
+                    SYNTH( lastAnswer[1] );
+                    SYNTH( lastAnswer[2] );
+                }
+
+                var svg = $.ajax('assets/svg/' + levels[state[0] - 1] + '/' + state[1] + '.svg');
+                $.wait(prevState[0] ? 1000 : 0).then(function() {
+                    $.when(svg, $('#level').fadeOut(prevState[0] ? undefined : 0)).done(function(svgDfr, animationDfr) {
+                        $('#level').html(document.importNode(svgDfr[0].documentElement, true)).fadeIn(function() {
+                            $('body').removeClass('whitenoise-fix');
+                            callback && callback();
+                        });
+
+                        _generatePathInfo();
+                        _scaleSVG();
+                    });
+                });
+            },
+            setup: (function() {
+                var setup = false;
+
+                return function() {
+                    if ( setup ) {
+                        return GAME;
+                    }
+                    $(window).resize(_scaleSVG);
+
+                    $('#level')
+                        .on('mouseenter', 'path', _pathEnter)
+                        .on('mouseleave', 'path', _pathLeave)
+                        .on('click',      'path', _pathClick);
+
+                    $('#help button').click(function() {
+                        $('body').addClass('whitenoise-fix');
+                        $('#help').fadeOut(function() {
+                            $(this).remove();
+                            $('#level path').filter(function() {
+                                return answers.jump[0][0] === notes[$(this).data('synaesthetic').color];
+                            }).attr('data-highlight', 'true').first().click();
+                            $('body').removeClass('whitenoise-fix');
+                        });
+                    });
+
+                    setup = true;
+
+                    return GAME;
+                }
+            })(),
+            end: function(callback) {
+                $('#level').fadeOut(function() { SYNTH( undefined, false ) });
+                $('#final').addClass('open');
+                $.ajax('assets/svg/endgame.svg').done(function(data) {
+                    $('#final').prepend(document.importNode(data.documentElement, true));
+                    _scaleSVG();
+                    callback && callback();
+                });
+
+                return GAME;
+            },
+            help: function() {
+                helpTimeout = setTimeout(function() {
+                    if ( ! $('#level').is(':visible') ) {
+                        return GAME.help();
+                    }
+                    $('#help').fadeIn();
+                }, 1000 * 15);
+            },
+            pastFirstLevel: $.cookie('level') > 1 || $.cookie('point') > 1
+        }
+    })();
+
+    $('.js-start').click(function() {
         var _this = this;
 
         if ( $(this).attr('data-continue') ) {
@@ -229,11 +244,21 @@ $(function() {
                 $('body').removeClass('whitenoise-fix');
             });
         } else {
-            GAME.setup().state(1, 1).level(function() {
+            GAME.setup().state($.cookie('level') || 1, $.cookie('point') || 1).level(function() {
                 $(_this).attr('data-continue', 'true').text('CONTINUE');
             });
-            $('#explanation').fadeIn();
+            if ( ! GAME.pastFirstLevel ) {
+                $('#explanation').fadeIn();
+            }
         }
+    });
+
+    $('.js-reset').click(function() {
+        GAME.pastFirstLevel = false;
+        GAME.setup().state(1, 1).level(function() {
+            $('.js-start').attr('data-continue', 'true').text('CONTINUE');
+        });
+        $('#explanation').fadeIn();
     });
 
     $('#explanation button').click(function() {
@@ -243,9 +268,16 @@ $(function() {
         setTimeout(function() {
             clearInterval(ellipsis);
             $('#explanation').fadeOut(function() { $(this).remove() });
-            GAME.help();
+            if ( ! GAME.pastFirstLevel ) {
+                GAME.help();
+            }
         }, 1999);
     });
+
+    if ( GAME.pastFirstLevel ) {
+        $('.js-start').text('CONTINUE');
+        $('body').addClass('played');
+    }
 
     $('#level-footer h1').click(function() {
         $(window).scrollTop(0);
